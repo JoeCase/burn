f2abbc564c28f158b6351fc6e140d3809454a4a8
jcase146 <jcase146@gmail.com>
2013-02-18 14:28:14 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Burn::Application.config.secret_token = '072f286d3dea65cb1b5057aff0b85c15da0bb7c81c2a73958a51f5162d33128dd8a467897929003ebb328f96fbdbaeccf4c10c61c067f46b633645472a86ba83'
